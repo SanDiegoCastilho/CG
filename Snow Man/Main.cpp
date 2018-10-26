634a53b0ccd284ca20bd8d0b8a0b290613f48191
SanDiegoCastilho <sandiegocastilho@gmail.com>
2018-10-26 14:11:35 -0300

    Primeiro Snow man \õ/

---
+++ Snow Man/Main.cpp
@@ -0,0 +1,74 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#define WidthWindow  500
+#define HeightWindow 500
+
+double Height = 1000;
+double Width  = 1000;
+
+void RayCasting(){
+	int Line, Column;
+	double x, y, Dx, Dy;
+
+	Dy = Height/HeightWindow;
+	Dx = Width/WidthWindow;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_POINTS);
+
+		for (Line = 0; Line < HeightWindow; Line ++){
+			y = Height/2 - Dy/2 - Line * Dy;
+
+			for (Column = 0; Column < WidthWindow; Column ++){
+				x = -Width/2 + Dx/2 + Column * Dx;
+
+				glColor3f(0.2, 0.5, 0.9);
+				glVertex2f(Column, Line);
+			}
+		}
+
+	glEnd();
+	glFlush();
+}
+
+void Resize(int w, int h){
+	int hSize = w * H/W;
+	int wSize = h * W/H;
+
+	if (h >= w) 
+		glViewport(0, (h - hSize)/2, w, hSize);
+	
+	else if (h < w) 
+		glViewport((w - wSize)/2, 0,  wSize, h);
+	
+ 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, WidthWindow, HeightWindow, 0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+int main(int argc, char **argv){
+
+	glutInit(argc, **argv);
+	glutInitContextProfile(GLUT_COMPATIBILY_PROFILE);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(WidthWindow, HeightWindow);
+	glutInitWindowPosition(0, 0);
+
+	glutCreateWindow("Snow Man");
+	glutDisplayFunc(RayCasting);
+	glutReshapeFunc(Resize);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	glutMainLoop();
+
+	glClearColor(0.8, 0.9, 0.9, 0.0);
+
+	return 0;
+}
